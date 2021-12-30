d67ffb81a5ac3a61087ebb66364d0e705ccd8c53
KIMYUNMEE <jayoo8665@naver.com>
2021-12-30 23:26:24 +0900

    검색어 비어있을 때 이벤트 발생 시 함수 실행 중

--- src/sub/Gallery.js
+++ src/sub/Gallery.js
@@ -2,10 +2,8 @@ import axios from "axios";
 import { useEffect, useState, useRef } from "react";
 import Masonry from "react-masonry-component";
 
-//public폴더의 절대경로를 변수에 저장
 const path=process.env.PUBLIC_URL;
 
-//masonry 개별 패널 옵션
 const masonryOptions = {
   fitWidth: false, 
   //columnWidth: 200,
@@ -14,34 +12,84 @@ const masonryOptions = {
 }
 
 function Gallery(){ 
-  let [items, setItems] = useState([]); 
-  //로딩바 출력 유무를 결정할 state생성하고 초기값을 true로 설정
+  let [items, setItems] = useState([]);   
   let [loading, setLoading] = useState(true);
-  let list = useRef(null);  
-  let [url, url2] = getURL();  
+  let [enableClick, setEnableClick] = useState(true);
+  let [interest, setInterest] = useState(true);
+  let list = useRef(null);
+  let input = useRef(null);
   
-  useEffect(()=>{    
-    getFlickr(url);
+  useEffect(()=>{ 
+    getFlickr({
+      type: "interest",
+      count: 500
+    });
   },[]);  
 
   return (
     <section className="content gallery">
       <div className="inner">
-        <h1 onClick={()=>{
-          list.current.classList.remove("on");
-          //기존 리스트가 아래로 사라질떄 다시 loading값 true로 변경
-          setLoading(true);
-          getFlickr(url);
-        }}>Gallery</h1>
-       
-        <button onClick={()=>{          
-          list.current.classList.remove("on");   
-          //기존 리스트가 아래로 사라질떄 다시 loading값 true로 변경
-          setLoading(true);       
-          getFlickr(url2);
-        }}>수정</button>
+        <h1 onClick={()=>{          
+          if(enableClick && !interest){ //모션이 끝났고 현재 interest값이 true가 아닐때 interst호출 기능 실행
+            setEnableClick(false);           
+            list.current.classList.remove("on");         
+            setLoading(true);
 
-        {/* 삼항 연산자로 loading state값이 true일 때에만 로딩이미지 출력 */}
+            getFlickr({
+              type: "interest",
+              count: 500
+            });
+          }          
+        }}>Gallery</h1>
+
+        <div className="searchBox">  
+          <input type="text" ref={input} onKeyPress={e=>{
+            const tags = input.current.value; 
+             
+            if(e.key !== "Enter" || tags==="") return;
+            if(enableClick){
+              setEnableClick(false);  
+              //검색어 입력하고 엔터키 눌렀을떄 interest값을 false로 변경해 제목클릭가능하게 설정
+              setInterest(false);          
+              list.current.classList.remove("on");         
+              setLoading(true);
+             
+              const tags = input.current.value;              
+              input.current.value = "";
+
+              getFlickr({
+                type: "search",
+                count: 500,
+                tags: tags 
+              });
+            }            
+          }} />
+          <button onClick={()=>{
+            if(enableClick){
+              const tags = input.current.value; 
+              if(tags==="") return;  
+
+              setEnableClick(false);  
+              //검색어 버튼 클릭시 interest값을 false로 변경해 제목클릭가능하게 설정
+              setInterest(false);             
+              list.current.classList.remove("on");         
+              setLoading(true);
+             
+                         
+              input.current.value = "";
+
+              getFlickr({
+                type: "search",
+                count: 500,
+                tags: tags 
+              });
+            }
+          }}>검색</button>
+        </div>      
+
+        
+
+       
         {(loading) ? <img className="loading" src={path+"/img/loading.gif"}/> : ""}
        
         <div className="list" ref={list}>         
@@ -72,30 +120,42 @@ function Gallery(){
     </section>
   )  
 
-  function getURL(){
+ 
+  
+  async function getFlickr(opt){ 
+    let url = "";
+    
     const baseURL = "https://www.flickr.com/services/rest/?";
     const method1 = "flickr.interestingness.getList";
     const method2 = "flickr.photos.search";
     const key= "e7ed3b39fe112d7e93d03c19325305e0";
-    const count = 500;
-    const url = `${baseURL}method=${method1}&api_key=${key}&per_page=${count}&format=json&nojsoncallback=1`;  
-    const url2 = `${baseURL}method=${method2}&api_key=${key}&per_page=${count}&format=json&nojsoncallback=1&tags=ocean`;
-    return [url, url2];
-  }
-  
-  async function getFlickr(url){    
+    const count = opt.count;
+    
+    if(opt.type === "interest"){
+      url = `${baseURL}method=${method1}&api_key=${key}&per_page=${count}&format=json&nojsoncallback=1`;  
+    }
+   
+    else if(opt.type === "search"){
+      url = `${baseURL}method=${method2}&api_key=${key}&per_page=${count}&format=json&nojsoncallback=1&tags=${opt.tags}`;
+    
+    }else{
+      console.error("api요청 타입을 interest, search중에서 지정하세요.");
+    }
+
     await axios
     .get(url)
     .then(json=>{      
       setItems(json.data.photos.photo);
     })    
     
-    //masonry ui가 적용될 시간을 벌기 위해서 1초뒤 리스트 활성화
+   
     setTimeout(()=>{
-      list.current.classList.add("on");
-      //데이터 호출이 끝나면 setLoading으로 loading state값 false로 변경
+      list.current.classList.add("on");      
       setLoading(false);
-    },1000);
+      setTimeout(()=>{
+        setEnableClick(true);
+      },1000); 
+    },1000); 
      
   }
 }
