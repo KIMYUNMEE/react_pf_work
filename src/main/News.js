12c2e5b3b33bb19de397f08e9ab5ad09c2001a31
KIMYUNMEE <jayoo8665@naver.com>
2022-01-23 23:42:28 +0900

    react포트폴리오 진행중

--- src/main/News.js
+++ src/main/News.js
@@ -1,20 +1,24 @@
 
 import { useEffect, useState } from "react/cjs/react.development";
 function News() {
+         
   
   const getLocalItems=()=>{
     let data = localStorage.getItem('posts');
 
-    if(data){
+    if (data) {
+      
       let result = JSON.parse(data);
       result = result.splice(0,6);
       return result;
-    }else{
-       return [
-        {title: 'Hello0', content: 'Here comes description in detail.'},
-        {title: 'Hello1', content: 'Here comes description in detail.'},  
-        {title: 'Hello2', content: 'Here comes description in detail.'},  
-        {title: 'Hello3', content: 'Here comes description in detail.'}    
+    } else {
+     
+      return [
+
+         {  sr:'../img/21dcee5f3518b517dd549d33ca245755.jpeg', title: 'Hello0', content: 'Here comes description in detail.'},
+        {sr:'../img/1b81f603faf67d4874078616a3e4827a.jpeg', title: 'Hello112345', content: 'Here comes description in detail.'},  
+        {sr:'../img/21dcee5f3518b517dd549d33ca245755.jpeg', title: 'Hello2', content: 'Here comes description in detail.'},  
+        {sr:'../img/21dcee5f3518b517dd549d33ca245755.jpeg', title: 'Hello3', content: 'Here comes description in detail.'}    
       ];
     }
     }
@@ -31,10 +35,12 @@ function News() {
         <h2>RECENT NEWS</h2>
         
         <div className="txtBox">
+
           {
             posts.map((post,index)=>{
               return (
                 <article key={index}>
+                  <img src={post.sr} alt="상자"></img>
                   <h3>{post.title}</h3>
                   <p>{post.content}</p>
                 </article>
