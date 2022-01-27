e872cec1bc49613d2de6ee2176498e296c6bd495
KIMYUNMEE <jayoo8665@naver.com>
2022-01-27 11:56:57 +0900

    react포트폴리오 처음 로딩바 생성: 처음 app컴포넌트가 index에 연결되면 스타일 mask(로딩바)를 remove해준다 app컴포넌트가 연결될때 발생하는 시간에 로딩아이콘을 보여주는것

--- src/App.js
+++ src/App.js
@@ -9,8 +9,22 @@ import Gallery from "./sub/Gallery.js";
 import Location from "./sub/Location.js";
 import Join from "./sub/Join.js";
 import Youtube from "./sub/Youtube.js";
+import { useEffect } from 'react';
+function App() {  
+  useEffect(()=>{
+    document.body.classList.remove('first');
 
-function App() {
+    const mask = document.querySelector('.mask');
+    if(mask){      
+      setTimeout(()=>{
+        mask.classList.add('off');
+        setTimeout(()=>{
+          mask.remove();
+        },1000); //transiton 1초동안 fadeout될때까지 기다리는 시간
+      },4000);//처음 App컴포넌트가 index.html에 연결될때까지의 시간 지연
+    } 
+
+  },[]);
   return (
     <div className="App"> 
   
